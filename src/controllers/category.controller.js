a14d9671c6433a6cafea3c745cc594b8f52211e0
Emon8794 <emonabujafor@gmail.com>
2025-05-03 00:07:49 +0600

    refactor: enhance category and dua retrieval with search functionality

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,8 +1,12 @@
 const db = require("../../connectDb.js");
 
 const getCategories = (req, res, next) => {
-  const sql = "select * from category";
-  const params = [];
+  const { q } = req.query || {};
+  const sql = `
+  select * from category
+  WHERE cat_name_bn like ? or cat_name_en like ?
+  `;
+  const params = [`%${q}%`, `%${q}%`];
   db.all(sql, params, (err, rows) => {
     if (err) {
       res.status(400).json({ error: err.message });
--- src/controllers/dua.controller.js
+++ src/controllers/dua.controller.js
@@ -1,8 +1,15 @@
 const db = require("../../connectDb");
 
 const getDuas = (req, res, next) => {
-  const sql = "select * from dua where cat_id = ?";
-  const params = [req.params.id];
+  const { q } = req.query || {};
+  const cat_id = req.params.id;
+  let sql = `
+  select * from dua where cat_id = ?`;
+  const params = [cat_id];
+  if (q?.trim()?.length > 0) {
+    sql += ` and (dua_name_bn like ? or dua_name_en like ?) `;
+    params.push(`%${q}%`, `%${q}%`);
+  }
   db.all(sql, params, (err, rows) => {
     if (err) {
       res.status(500).json({ error: "Error retrieving duas." });
